268ed2090d1e210f16851a2ee07307485acb929a
matteolpera <matteo.lepera02@gmail.com>
2025-11-11 16:30:21 +0100

    feat: wip form

--- script.js
+++ script.js
@@ -52,7 +52,7 @@ let cardOutput = "";
 for (let i = 0; i < teamMembers.length; i++) {
   const curTeamMembers = teamMembers[i];
   const { name, role, email, img } = curTeamMembers;
-  const card = ` <div class="team-card col-30 mt-30 d-flex text-center align-center g-5">
+  const card = ` <div class="team-card mt-30 d-flex text-center align-center g-5">
                 <div class="member-image pl-5">
                     <img src="${img}" alt="...">
                 </div>
